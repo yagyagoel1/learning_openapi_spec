bf79c2ddfeac763a7d576aca6d3c14e98e8a9082
yagyagoel1 <yagyagoel87@gmail.com>
2024-09-18 19:40:32 +0530

    learned about the ndoe-client and swagger-cleint

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import express ,{json,urlencoded} from "express";
 import {RegisterRoutes}from "../build/routes";
+// src/app.ts
+
+import { Response as ExResponse, Request as ExRequest } from "express";
+import swaggerUi from "swagger-ui-express";
+
+// ...
+
+
 export const app = express();
 
 
@@ -9,5 +17,9 @@ app.use(urlencoded({
 
 app.use(json())
 
-
+app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+    return res.send(
+      swaggerUi.generateHTML(await import("../build/swagger.json"))
+    );
+  }); 
 RegisterRoutes(app)
